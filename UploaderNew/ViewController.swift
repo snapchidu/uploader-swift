9f8aa736c1374af0ebfcba30883a0d63ac909c2f
Alex Handy <handy437@gmail.com>
2015-07-01 19:15:02 +0100

    Initial Commit

---
+++ UploaderNew/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UploaderNew
+//
+//  Created by Alexander Handy on 01/07/2015.
+//  Copyright (c) 2015 StitchVid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
